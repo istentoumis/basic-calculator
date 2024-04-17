a1742471aed83119aafafb155f82ce0abaad858b
Ilias <istentoumis9@gmail.com>
2024-04-17 20:58:02 +0300

    styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,31 @@ import './App.css';
 function App() {
   return (
     <div className="calculator">
+      <h2>Basic Calculator</h2>
+      <div className="calculator-frame">
+        <input type="text" disabled />
+        <input type="text" disabled />
+        <div className="buttons">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>*</button>
+          <button>C</button>
+          <button>0</button>
+          <button>=</button>
+          <button>/</button>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
