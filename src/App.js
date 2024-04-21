7daf445b43173a4aa6d88018bb9c19c9edfa0faa
Ilias <istentoumis9@gmail.com>
2024-04-21 18:07:53 +0300

    more buttons

--- src/App.js
+++ src/App.js
@@ -8,22 +8,26 @@ function App() {
         <input type="text" disabled />
         <input type="text" disabled />
         <div className="buttons">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>*</button>
-          <button>C</button>
-          <button>0</button>
-          <button>=</button>
-          <button>/</button>
+          <button onClick>%</button>
+          <button onClick>CE</button>
+          <button onClick>C</button>
+          <button onClick>del</button>
+          <button onClick>7</button>
+          <button onClick>8</button>
+          <button onClick>9</button>
+          <button onClick>/</button>
+          <button onClick>4</button>
+          <button onClick>5</button>
+          <button onClick>6</button>
+          <button onClick>*</button>
+          <button onClick>1</button>
+          <button onClick>2</button>
+          <button onClick>3</button>
+          <button onClick>+</button>
+          <button onClick>x<sup>2</sup></button>
+          <button onClick>0</button>
+          <button onClick>.</button>
+          <button onClick>=</button>
         </div>
       </div>
     </div>
